b85e4c261efbc3604cde084fd9a618a1841cecf0
Shreyas Sharma <shreysh2@cisco.com>
2023-10-09 23:48:58 +0530

    fixed-application-initilised-calling-registered-lines

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-const Webex = require("webex");
 const Calling = require("webex/calling");
-// const { Calling } = require("webex");
 
 let callingClient;
 let line;
-// let webex;
 let calling;
-
-console.log("Shreyas", Webex);
-console.log("Shreyas 2", Calling);
-
 const webexConfig = {
   config: {
     logger: {
@@ -36,7 +29,6 @@ const webexConfig = {
 };
 
 // Set calling configuration
-
 const callingConfig = {
   clientConfig: {
     calling: true,
@@ -54,43 +46,29 @@ const callingConfig = {
     level: "info",
   },
 };
-// Create Calling object
-// console.log(
-//   "Creating webex---------------------------------------------------------"
-// );
-// webex = Webex.init(webexConfig);
-// webex.once("ready", () => {
-//   console.log("Authentication#Shreyas() :: Webex Ready");
-// });
-// .then(() => {
-// });
 
-// callingClient = Calling.init(webex, callingConfig);
-// console.log(callingClient);
-//   .then((response) => {
-//     console.log("calling client", response);
-//   })
-//   .catch((e) => {
-//     console.log("calling client error", e);
-//   });
-calling = Calling.init({ webexConfig, callingConfig });
-calling.on("ready", () => {
-  calling.register().then(() => {
-    callingClient = calling.callingClient;
-    console.log("Registered Shreyas");
+async function callingInit() {
+  console.log("Called calling init");
+  calling = await Calling.default.init({
+    webexConfig,
+    callingConfig,
   });
-});
-// console.log(
-//   "Client created---------------------------------------------------------",
-//   callingClient
-// );
+  calling.on("ready", () => {
+    calling.register().then(() => {
+      callingClient = calling.callingClient;
+    });
+  });
+}
 
-// line = Object.values(callingClient.getLines())[0];
+async function registerLine() {
+  line = Object.values(callingClient.getLines())[0];
 
-// // Listen to the registered event from line
+  line.on("registered", (lineInfo) => {
+    console.log("Line information: ", lineInfo);
+  });
 
-// line.on("registered", (lineInfo) => {
-//   console.log("Line information: ", lineInfo);
-// });
+  line.register();
+}
 
-// line.register();
+window.registerLine = registerLine;
+window.callingInit = callingInit;
